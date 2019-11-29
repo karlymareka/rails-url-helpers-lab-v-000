abe4a0e47db22bd66c0b6ff5897cfce78c9604b0
Karly Mareka <karly.mareka@gmail.com>
2019-11-29 04:02:48 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,6 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
-<<<<<<< HEAD
 
   def activate
     @student = Student.find(params[:id])
